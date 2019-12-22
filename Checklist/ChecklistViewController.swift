ee3635ecf85ff2a8e81b53c85595e80db9e57b60
elisha-enu <elishaenu@gmail.com>
2019-12-22 23:54:09 +0700

    creating more rows

--- Checklist/ChecklistViewController.swift
+++ Checklist/ChecklistViewController.swift
@@ -17,7 +17,7 @@ class ChecklistViewController: UITableViewController  {
 
     // to decide how many cells will display
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 1000
     }
     
     // provide the actual cell
@@ -26,6 +26,21 @@ class ChecklistViewController: UITableViewController  {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // dont miss spele of identifier, the table view won;t be able to find the cell
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        
+        if let label = cell.viewWithTag(1000) as? UILabel {
+            if indexPath.row % 5 == 0 {
+                label.text = "Take a jog"
+            } else if indexPath.row % 4 == 0 {
+                label.text = "Watch a movie"
+            } else if indexPath.row % 3 == 0 {
+                label.text = "Code an app"
+            } else if indexPath.row % 2 == 0 {
+                label.text = "Walk the dog"
+            } else if indexPath.row % 1 == 0 {
+                label.text = "Study design patterns"
+            }
+        }
+        
         return cell
     }
 
