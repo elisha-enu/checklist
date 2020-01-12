d0a96c5b8620b96301c5f6e6794b917973e132e8
elisha-enu <elishaenu@gmail.com>
2020-01-12 16:00:51 +0700

    control events

--- Checklist/AddItemTableViewController.swift
+++ Checklist/AddItemTableViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class AddItemTableViewController: UITableViewController {
     
     
+    @IBOutlet weak var cancelBarButton: UIBarButtonItem!
+    @IBOutlet weak var addBarButton: UIBarButtonItem!
     @IBOutlet weak var textField: UITextField!
     
     @IBAction func cancel(_ sender: Any) {
@@ -19,7 +21,7 @@ class AddItemTableViewController: UITableViewController {
     
     @IBAction func done(_ sender: Any) {
         navigationController?.popViewController(animated: true)
-        print("Content of the field typed is \(textField.text)")
+//        print("Content of the field typed is \(textField.text)")
     }
     
     override func viewDidLoad() {
@@ -43,5 +45,22 @@ extension AddItemTableViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return false
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let oldText = textField.text,
+            let stringRange = Range(range, in: oldText) else {
+                return false
+        }
+        
+        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+        
+        if newText.isEmpty {
+            addBarButton.isEnabled = false
+        } else {
+            addBarButton.isEnabled = true
+        }
+        
+        return true
     }
 }
